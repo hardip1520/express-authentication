2b9d724e4a2749d29b201cc9d60912bdb5e6dba2
hardip1520 <60613772+hardip1520@users.noreply.github.com>
2023-02-28 10:14:51 -0500

    Update db.js

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,5 @@
 module.exports = 
 {
-    "URI":"mongodb://localhost/business-list"
+     // "URI":"mongodb://localhost/business-list"
+     "URI" :"mongodb+srv://hardipsinhmatroja1520:zLjbtcLU99QThM2c@cluster0.5b9sjdh.mongodb.net/business-contact?retryWrites=true&w=majority"
 }
